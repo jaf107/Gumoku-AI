1a4f75985596400d1f38c013863c977ef5d78e81
jsureka <bsse1115@iit.du.ac.bd>
2022-08-27 04:27:04 +0600

    Added a modal for quitting app

--- gomoku.py
+++ gomoku.py
@@ -1,6 +1,7 @@
 import time
 import math
-
+from tkinter import *
+from tkinter import ttk
 import numpy as np
 import pygame
 
@@ -186,7 +187,6 @@ class Engine:
     @classmethod
     def get_consecutive_score(cls, pattern_dict):
         score = 0
-        print(pattern_dict)
         for pattern in pattern_dict:
             if pattern.count('X') == 5:
                 if pattern[0] == 'O' and pattern[-1] == 'O':
@@ -415,12 +415,12 @@ class GomokuUI():
         self.height = 30 * size + 30 + 150
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.font = pygame.font.SysFont("arial", 24)
-        board = Board(size=size)
+        self.board = Board(size=size)
 
         # self.mode = mode
         self.going = True
         self.clock = pygame.time.Clock()
-        self.game = Game(board)
+        self.game = Game(self.board)
 
     def loop(self):
         while self.going:
@@ -451,8 +451,39 @@ class GomokuUI():
         self.game.black_score = Engine.get_score(self.game.board, True, not self.game.black_turn)
         self.game.white_score = Engine.get_score(self.game.board, False, not self.game.black_turn)
 
-    def restartBox():
-        self
+    def restartBox(self):
+            # Create an instance of tkinter frame
+        win = Tk()
+
+        # Define a function to implement choice function
+        def choice(option):
+            pop.destroy()
+            if option == "yes":
+                pygame.quit()
+            else:
+                return
+        global pop
+        pop = win
+        pop.title("Confirmation")
+        pop.geometry("350x200")
+        pop.config(bg="white")
+        # Create a Label Text
+        label = Label(pop, text="Winner is Black" if self.game.winner == 2 else "Winner is White",
+        font=('Aerial', 12))
+        label.pack(pady=20)
+        label = Label(pop, text="Would You like to Quit?",
+        font=('Aerial', 12))
+        label.pack(pady=20)
+        # Add a Frame
+        frame = Frame(pop, bg="gray71")
+        frame.pack(pady=10)
+        # Add Button for making selection
+        button1 = Button(frame, text="Yes", command=lambda: choice("yes"), bg="blue", fg="white")
+        button1.grid(row=0, column=1)
+        button2 = Button(frame, text="No", command=lambda: choice("no"), bg="blue", fg="white")
+        button2.grid(row=0, column=2)
+        win.mainloop()
+
 
     def draw(self):
         self.screen.fill((255, 255, 255))
@@ -461,6 +492,7 @@ class GomokuUI():
         self.game.draw(self.screen)
         if self.game.game_over:
             self.screen.blit(self.font.render("{0} Win".format("Black" if self.game.winner == 2 else "White"), True, (0, 0, 0)), (self.width // 4 * 3, 10))
+            self.restartBox()
 
         pygame.display.update()
 
